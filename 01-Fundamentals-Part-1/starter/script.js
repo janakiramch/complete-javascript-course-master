e38e183109cb4d15eea19f2bc29bebb82939c8b0
Ch JanakiRam <chjanakiram99@gmail.com>
2023-01-15 23:14:13 -0500

    section 1 coding challenge 1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -12,3 +12,16 @@ console.log(
     " with population of " +
     myCountryPopulation
 );
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const bmiMark = massMark / heightMark ** 2;
+const bmiJohn = massJohn / (heightJohn * heightJohn);
+
+const higherBmi = bmiMark - bmiJohn;
+
+console.log(bmiMark, bmiJohn);
+console.log("Higher BMI is " + bmiMark);
